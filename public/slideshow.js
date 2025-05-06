df779d60c918edbc9e6e082f6c04119a654a2bfd
humayramahboob <104864864+humayramahboob@users.noreply.github.com>
2025-05-06 10:08:04 +0000

    modified slideshow

--- public/slideshow.js
+++ public/slideshow.js
@@ -90,6 +90,11 @@ imagesWithDetails.forEach((item) => {
 
 const swiper = new Swiper(".swiper", {
   loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  speed: 1200,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
